18a87cb8fad57801726e80e71211fd179a5e4da7
Tolu Joel <toluisjoel@gmail.com>
2025-04-09 17:55:28 +0100

    chore: Remove unused functions

--- utils.go
+++ utils.go
@@ -20,19 +20,3 @@ func nullTimeToTime(nt sql.NullTime) time.Time {
 	}
 	return time.Time{} // Zero time
 }
-
-// Convert sql.NullInt64 to int64
-func nullInt64ToInt64(ni sql.NullInt64) int64 {
-	if ni.Valid {
-		return ni.Int64
-	}
-	return 0
-}
-
-// Convert sql.NullBool to bool
-func nullBoolToBool(nb sql.NullBool) bool {
-	if nb.Valid {
-		return nb.Bool
-	}
-	return false
-}
