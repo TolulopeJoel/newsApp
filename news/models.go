385ff8f947c075ba9228921119671f2f926e674d
Tolu Joel <toluisjoel@gmail.com>
2025-04-14 12:31:32 +0100

    feat: Add `Source` model struct
    - add `DatabaseSourceToSource` helper function
    - add `DatabaseSourcesToSources` helper function

--- news/models.go
+++ news/models.go
@@ -17,6 +17,12 @@ type Article struct {
 	UpdatedAt   time.Time
 }
 
+type Source struct {
+	ID      int32 `json:"id"`
+	Name    string
+	FeedUrl string
+}
+
 func DatabaseArticleToArticle(article database.Article) Article {
 	return Article{
 		ID:          article.ID,
@@ -34,6 +40,22 @@ func DatabaseArticlesToArticles(articles []database.Article) []Article {
 	result := make([]Article, len(articles))
 	for _, article := range articles {
 		result = append(result, DatabaseArticleToArticle(article))
+	}
+	return result
+}
+
+func DatabaseSourceToSource(source database.Source) Source {
+	return Source{
+		ID:      source.ID,
+		Name:    source.Name.String,
+		FeedUrl: source.FeedUrl.String,
+	}
+}
+
+func DatabaseSourcesToSources(sources []database.Source) []Source {
+	result := make([]Source, len(sources))
+	for _, source := range sources {
+		result = append(result, DatabaseSourceToSource(source))
 	}
 	return result
 }
